c12a04e349b16d03a01dab020c23c2cb06b50165
Takaaki Matsubara <matsubara@seitoku-jp.com>
2018-03-05 18:23:54 +0900

    1st commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = "0";
+            string a = string.Empty;
+            Console.WriteLine(string.Format("{0},{1}", a, b));
+            Console.ReadKey();
+        }
+    }
+}
